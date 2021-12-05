7a82dca7cd693ce42868c99763a7eb9be5fb7cb2
maedeh <emamimaedeh7481@gmail.com>
2021-12-05 18:31:04 +0330

    +class

--- git.study/Source.cpp
+++ git.study/Source.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-#include <vector>
-using std::vector;
+#include <map>
+using std::map;
 #include <string>
 using std::string;
 
@@ -10,16 +10,23 @@ int main() {
 
 	cout << "welcom\n" << endl;
 
-	vector<string>student(5);
-	student[0] = "zara\n";
-	student[1] = "afra\n";
-	student[2] = "neda\n";
-	student[3] = "taraneh\n";
-	student[4] = "maryam";
+	map<int, string>name;
+	name[0] = "zara";
+	name[1] = "afra";
+	name[2] = "neda";
+	name[3] = "taraneh";
+	name[4] = "maryam";
 
-	for (int i = 0; i < student.size(); i++)
+	map<int, int>age;
+	age[0] = 18;
+	age[1] = 22;
+    age[2] = 16;
+	age[3] = 20;
+	age[4] = 21;
+
+	for (int i = 0; i < name.size(); i++)
 	{
-		cout << student[i];
+		cout << name[i] << "=" << age[i] << endl;
 	}
 
 	return 0;
